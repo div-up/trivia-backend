aaf3ffd8ffa7fbed9829957c29d50611725171d4
Divyanshu Upadhyay <divyanshukr786@gmail.com>
2025-04-24 16:36:56 +0530

    added frontend url

--- server.js
+++ server.js
@@ -9,10 +9,12 @@ connectDB();
 
 const app = express();
 
+const allowedOrigins = ["http://localhost:5173","https://triviaquizwhiz.netlify.app/"]
+
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: allowedOrigins,
     credentials: true,
   })
 );
